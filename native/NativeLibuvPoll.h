741286e9ad95aaa611612fdcfd453f73de1cb4b1
Chris Speciale <christopher.gaf@gmail.com>
2026-04-28 02:04:37 -0400

    Avoid hxcpp include from native libuv header

--- native/NativeLibuvPoll.h
+++ native/NativeLibuvPoll.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <hxcpp.h>
-
 Dynamic crossbyte_libuv_poll_create(int capacity);
 void crossbyte_libuv_poll_prepare(Dynamic handle, Array<Dynamic> read, Array<Dynamic> write);
 Array<Dynamic> crossbyte_libuv_poll_events(Dynamic handle, double timeout);
