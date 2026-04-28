d7e9ee596abaccec5ccdc8ada12d11d234231238
Chris Speciale <christopher.gaf@gmail.com>
2026-04-28 01:59:16 -0400

    Fix native libuv build integration

--- src/crossbyte/libuv/NativeLibuvPoll.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#include "../../../native/NativeLibuvPoll.h"
--- src/crossbyte/libuv/_internal/NativeLibuvPoll.hx
+++ src/crossbyte/libuv/_internal/NativeLibuvPoll.hx
@@ -1,8 +1,27 @@
 package crossbyte.libuv._internal;
 
 #if (cpp && crossbyte_libuv_native)
-@:buildXml('<include name="${haxelib:crossbyte-libuv}/native/NativeLibuvPollBuild.xml"/>')
-@:include("crossbyte/libuv/NativeLibuvPoll.h")
+@:buildXml("
+<files id='haxe'>
+	<compilerflag value='-I${haxelib:crossbyte-libuv}/native'/>
+	<compilerflag value='-I${LIBUV_INCLUDE}' if='LIBUV_INCLUDE'/>
+	<file name='${haxelib:crossbyte-libuv}/native/NativeLibuvPoll.cpp'>
+		<depend name='${haxelib:crossbyte-libuv}/native/NativeLibuvPoll.h'/>
+	</file>
+</files>
+
+<target id='haxe'>
+	<lib name='-L${LIBUV_LIB}' if='LIBUV_LIB' unless='windows'/>
+	<lib name='-luv' unless='windows'/>
+	<flag value='-libpath:${LIBUV_LIB}' if='LIBUV_LIB'/>
+	<lib name='uv.lib' if='windows'/>
+	<lib name='ws2_32.lib' if='windows'/>
+	<lib name='iphlpapi.lib' if='windows'/>
+	<lib name='psapi.lib' if='windows'/>
+	<lib name='userenv.lib' if='windows'/>
+</target>
+")
+@:include("NativeLibuvPoll.h")
 extern class NativeLibuvPoll {
 	@:native("crossbyte_libuv_poll_create")
 	public static function create(capacity:Int):Dynamic;
